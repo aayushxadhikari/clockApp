86455dfe59c47c8677466838f8ef2bcfb0ba57c9
aayushxadhikari <aayushxadhikari@gmail.com>
2024-12-29 21:57:15 +0545

    Adding in homePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:alarmclock/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 class Homepage extends StatefulWidget {
@@ -19,15 +20,7 @@ class _HomepageState extends State<Homepage> {
     var offsetSign = timezoneString.startsWith('-') ? '' : '+';
 
     return Scaffold(
-      appBar: AppBar(backgroundColor: const Color(0xFF2D2F41),
-      elevation: 0,
-        title: const Text(
-          "Clock App",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
       backgroundColor: const Color(0xFF2D2F41),
-      
       body: Row(
         children: [
           Container(
@@ -36,23 +29,10 @@ class _HomepageState extends State<Homepage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: const Color(0xFF1A1C29),
-                    shadowColor: Colors.transparent,
-                  ),
-                  onPressed: () {},
-                  child: Column(
-                    children: const [
-                      FlutterLogo(),
-                      SizedBox(height: 8),
-                      Text(
-                        'Clock',
-                        style: TextStyle(color: Colors.white, fontSize: 14),
-                      ),
-                    ],
-                  ),
-                ),
+                buildMenuButton('Clock', 'assets/clock.png'),
+                buildMenuButton('Alarm', 'assets/alarm.png'),
+                buildMenuButton('Timer', 'assets/timer.png'),
+                buildMenuButton(' Stop\nWatch', 'assets/stopwatch.png'),
               ],
             ),
           ),
@@ -67,9 +47,9 @@ class _HomepageState extends State<Homepage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
+                  Text(
                     "Clock",
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                    style: GoogleFonts.lato(color: Colors.white, fontSize: 24),
                   ),
                   const SizedBox(height: 16),
                   Text(
@@ -85,9 +65,9 @@ class _HomepageState extends State<Homepage> {
                   // Replace ClockView with your actual implementation
                   ClockView(),
                   const SizedBox(height: 32),
-                  const Text(
+                  Text(
                     "Timezone",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    style: GoogleFonts.lato(color: Colors.white, fontSize: 20),
                   ),
                   const SizedBox(height: 16),
                   Row(
@@ -96,7 +76,7 @@ class _HomepageState extends State<Homepage> {
                       const SizedBox(width: 16),
                       Text(
                         'UTC$offsetSign$timezoneString',
-                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                        style: GoogleFonts.lato(color: Colors.white, fontSize: 16),
                       ),
                     ],
                   ),
@@ -106,6 +86,29 @@ class _HomepageState extends State<Homepage> {
           ),
         ],
       ),
+    );
+  }
+
+  Padding buildMenuButton(String title, String image) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, backgroundColor: const Color(0xFF1A1C29),
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {},
+                  child: Column(
+                    children: [
+                      Image.asset(image, scale: 1.5,),
+                      const SizedBox(height: 16),
+                      Text(
+                        title ?? '',
+                        style: GoogleFonts.lato(color: Colors.white, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
     );
   }
 }
